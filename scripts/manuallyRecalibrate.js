c1d2611c4e2a4a5ad6860de39c687de01d106312
Joshua Hong <joshua@kotharigroup.com>
2018-06-18 11:35:41 -0400

    Reads moves notation, observes move changes and on game completion

--- scripts/manuallyRecalibrate.js
+++ scripts/manuallyRecalibrate.js
@@ -3,8 +3,8 @@ function onError(error) {
 }
 
 function onGot(item) {
-	localStorage.setItem(item.activeTabID, item.activeTabID);
-	alert(localStorage.getItem(item.activeTabID));
+	//localStorage.setItem(item.activeTabID, item.activeTabID);
+	//alert(localStorage.getItem(item.activeTabID));
 
 	if (item.addonIsEnabled) {
 	} else {
@@ -13,3 +13,30 @@ function onGot(item) {
 
 var testing = browser.storage.local.get();
 testing.then(onGot, onError);
+
+
+//GETS MOVES
+var move = document.querySelectorAll('move');
+for( var i = 0; i < move.length; i++ ) {
+	//alert(move[i].innerHTML);
+}
+
+
+//GETS Changes
+var target = document.querySelector('.moves');
+ 
+
+var observer = new MutationObserver(function(mutations) {
+	if (document.querySelector('.result_wrap') != null) {
+		alert("END GAME");
+	} else {
+		alert("changed");
+	}
+});
+
+var config = { attributes: true, childList: true, characterData: true }
+ 
+observer.observe(target, config);
+
+// later, you can stop observing
+//observer.disconnect();
