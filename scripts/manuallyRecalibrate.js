70e08777f19548686bacd37e6189debd1c5b0bf4
Joshua Hong <joshua@kotharigroup.com>
2018-06-18 14:01:07 -0400

    listens for storage changes

--- scripts/manuallyRecalibrate.js
+++ scripts/manuallyRecalibrate.js
@@ -5,38 +5,46 @@ function onError(error) {
 function onGot(item) {
 	//localStorage.setItem(item.activeTabID, item.activeTabID);
 	//alert(localStorage.getItem(item.activeTabID));
+	//above unnecessary due to new way of seeing peices in lichess
 
 	if (item.addonIsEnabled) {
+		//alert("ENABLED");
 	} else {
+		//alert("DISABLED");
 	}
 }
 
-var testing = browser.storage.local.get();
-testing.then(onGot, onError);
+
+
+
+function logStorageChange(changes, area) {
+	var testing = browser.storage.local.get();
+	testing.then(onGot, onError);
+}
+
+browser.storage.onChanged.addListener(logStorageChange);
+
 
 
 //GETS MOVES
-var move = document.querySelectorAll('move');
+var move = document.querySelectorAll("move");
 for( var i = 0; i < move.length; i++ ) {
 	//alert(move[i].innerHTML);
 }
 
 
 //GETS Changes
-var target = document.querySelector('.moves');
- 
-
 var observer = new MutationObserver(function(mutations) {
-	if (document.querySelector('.result_wrap') != null) {
+	if (document.querySelector(".result_wrap") != null) {
 		alert("END GAME");
 	} else {
-		alert("changed");
+		alert("Changed");
 	}
 });
 
 var config = { attributes: true, childList: true, characterData: true }
  
-observer.observe(target, config);
+observer.observe(document.querySelector(".moves"), config);
 
 // later, you can stop observing
 //observer.disconnect();
