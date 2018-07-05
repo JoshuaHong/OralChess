c3037ac73b2f69330ee8305e0fd21c05e9b55396
Joshua Hong <joshua@kotharigroup.com>
2018-07-05 11:37:28 -0400

    Updated commands

--- scripts/recalibrate.js
+++ scripts/recalibrate.js
@@ -1,4 +1,12 @@
 var commands = ["knight", "bishop", "rook", "queen", "king",
+				"a takes", "a",
+				"b takes", "b",
+				"c takes", "c",
+				"d takes", "d",
+				"e takes", "e",
+				"f takes", "f",
+				"g takes", "g",
+				"h takes", "h",
 				"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
 				"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
 				"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
@@ -23,7 +31,66 @@ var isChrome;
 var addonIsEnabled;
 var blindfoldIsEnabled;
 var confirmationIsEnabled;
-var textToSpeechIsEnabled;
+var textToSpeechIsEnabled;window.addEventListener('load', function () {
+  // At first, let's check if we have permission for notification
+  // If not, let's ask for it
+  if (window.Notification && Notification.permission !== "granted") {
+    Notification.requestPermission(function (status) {
+      if (Notification.permission !== status) {
+        Notification.permission = status;
+      }
+    });
+  }
+
+  var button = document.getElementsByTagName('button')[0];
+
+  button.addEventListener('click', function () {
+    // If the user agreed to get notified
+    // Let's try to send ten notifications
+    if (window.Notification && Notification.permission === "granted") {
+      var i = 0;
+      // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
+      var interval = window.setInterval(function () {
+        // Thanks to the tag, we should only see the "Hi! 9" notification 
+        var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
+        if (i++ == 9) {
+          window.clearInterval(interval);
+        }
+      }, 200);
+    }
+
+    // If the user hasn't told if he wants to be notified or not
+    // Note: because of Chrome, we are not sure the permission property
+    // is set, therefore it's unsafe to check for the "default" value.
+    else if (window.Notification && Notification.permission !== "denied") {
+      Notification.requestPermission(function (status) {
+        // If the user said okay
+        if (status === "granted") {
+          var i = 0;
+          // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
+          var interval = window.setInterval(function () {
+            // Thanks to the tag, we should only see the "Hi! 9" notification 
+            var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
+            if (i++ == 9) {
+              window.clearInterval(interval);
+            }
+          }, 200);
+        }
+
+        // Otherwise, we can fallback to a regular modal alert
+        else {
+          alert("Hi!");
+        }
+      });
+    }
+
+    // If the user refuses to get notified
+    else {
+      // We can fallback to a regular modal alert
+      alert("Hi!");
+    }
+  });
+});
 
 var recognition = new webkitSpeechRecognition();
 recognition.lang = 'en-US';
