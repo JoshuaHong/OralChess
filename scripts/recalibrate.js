149e0c7608c78b06d6b4cf45cb9108227d1b95f1
Joshua Hong <Joshua99Hong@gmail.com>
2018-07-06 21:22:28 -0400

    Blindfold voice recognition now works

--- scripts/recalibrate.js
+++ scripts/recalibrate.js
@@ -31,66 +31,7 @@ var isChrome;
 var addonIsEnabled;
 var blindfoldIsEnabled;
 var confirmationIsEnabled;
-var textToSpeechIsEnabled;window.addEventListener('load', function () {
-  // At first, let's check if we have permission for notification
-  // If not, let's ask for it
-  if (window.Notification && Notification.permission !== "granted") {
-    Notification.requestPermission(function (status) {
-      if (Notification.permission !== status) {
-        Notification.permission = status;
-      }
-    });
-  }
-
-  var button = document.getElementsByTagName('button')[0];
-
-  button.addEventListener('click', function () {
-    // If the user agreed to get notified
-    // Let's try to send ten notifications
-    if (window.Notification && Notification.permission === "granted") {
-      var i = 0;
-      // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-      var interval = window.setInterval(function () {
-        // Thanks to the tag, we should only see the "Hi! 9" notification 
-        var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
-        if (i++ == 9) {
-          window.clearInterval(interval);
-        }
-      }, 200);
-    }
-
-    // If the user hasn't told if he wants to be notified or not
-    // Note: because of Chrome, we are not sure the permission property
-    // is set, therefore it's unsafe to check for the "default" value.
-    else if (window.Notification && Notification.permission !== "denied") {
-      Notification.requestPermission(function (status) {
-        // If the user said okay
-        if (status === "granted") {
-          var i = 0;
-          // Using an interval cause some browsers (including Firefox) are blocking notifications if there are too much in a certain time.
-          var interval = window.setInterval(function () {
-            // Thanks to the tag, we should only see the "Hi! 9" notification 
-            var n = new Notification("Hi! " + i, {tag: 'soManyNotification'});
-            if (i++ == 9) {
-              window.clearInterval(interval);
-            }
-          }, 200);
-        }
-
-        // Otherwise, we can fallback to a regular modal alert
-        else {
-          alert("Hi!");
-        }
-      });
-    }
-
-    // If the user refuses to get notified
-    else {
-      // We can fallback to a regular modal alert
-      alert("Hi!");
-    }
-  });
-});
+var textToSpeechIsEnabled;
 
 var recognition = new webkitSpeechRecognition();
 recognition.lang = 'en-US';
@@ -150,8 +91,16 @@ recognition.onresult = function(event) {
 			} else if (command == "blindfold") {
 				if (blindfoldIsEnabled) {
 					disableBlindfold();
+					chrome.storage.local.set({
+						blindfoldIsEnabled: false
+					});
+					blindfoldIsEnabled = false;
 				} else {
 					enableBlindfold();
+					chrome.storage.local.set({
+						blindfoldIsEnabled: true
+					});
+					blindfoldIsEnabled = true;
 				}
 			}else if (command == "0-0") {
 				document.dispatchEvent(new KeyboardEvent('keydown',{'keyCode':13,'which':13}));
@@ -520,13 +469,8 @@ function getCommand(command) {
 
 	for (var i = 0; i < command.length; i++) {
 		if (!isSquare(command[i])) {
-			if (!(command[i].length == 3 && ((command[i][0].charCodeAt(0) >= 97 && command[i][0].charCodeAt(0) <= 104) ||
-				(command[i][0].charCodeAt(0) >= 49 && command[i][0].charCodeAt(0) <= 57)) &&
-				(command[i][1].charCodeAt(0) >= 97 && command[i][1].charCodeAt(0) <= 104) &&
-				(command[i][2].charCodeAt(0) >= 49 && command[i][2].charCodeAt(0) <= 57))) {
-					command.splice(i, 1);
-					i--;
-			}
+			command.splice(i, 1);
+			i--;
 		}
 	}
 
@@ -585,10 +529,6 @@ function disableBlindfold() {
 	document.querySelectorAll(".king")[0].style.display = "block";
 	document.querySelectorAll(".king")[1].style.display = "block";
 }
-
-setTimeout(function() {
-
-}, 3000);
 
 
 /*
