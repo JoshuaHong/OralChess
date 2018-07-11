b1468ee2c5d9e79f3e7927f87c00484cfaf47362
Joshua Hong <joshua@kotharigroup.com>
2018-07-11 15:49:31 -0400

    Working on icons

--- menu/menu.js
+++ menu/menu.js
@@ -94,6 +94,13 @@ chrome.tabs.getSelected(null, function(tab){
 	}
 });
 
+/*chrome.browserAction.setIcon({
+	path : {
+		"16": "icons/iconEnabled16.png",
+		"24": "icons/iconEnabled24.png",
+		"32": "icons/iconEnabled32.png"
+	}
+});*/
 
 /*
 	Edits popup menu components based on previously stored data
