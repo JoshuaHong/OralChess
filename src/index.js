2352904f5adc9481adca031449a4cbdd3a34bfbc
Joshua Hong <Joshua99Hong@gmail.com>
2018-07-21 23:11:36 -0400

    Grammar typo

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ var grammars = ["knight", "N",
 				"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
 				"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
 				"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
-				"e1", "e2", "e3", "e4", "e5", "e6", "e7", "d8",
+				"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
 				"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
 				"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
 				"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
@@ -37,7 +37,7 @@ var commands = ["N", "B", "R", "Q", "K",
 				"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
 				"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
 				"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
-				"e1", "e2", "e3", "e4", "e5", "e6", "e7", "d8",
+				"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
 				"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
 				"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
 				"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
@@ -233,6 +233,7 @@ recognition.onresult = function(event) {
 
 					//Speaks command request if text to speech enabled
 					if (textToSpeechIsEnabled || textToSpeechIsEnabled == null) {
+						speechSynthesis.cancel();
 						speak(command, false);
 					}
 
