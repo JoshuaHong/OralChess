5ea90cbe4f6b734a7b73da7b6065904b09f3cb0d
Joshua Hong <Joshua99Hong@gmail.com>
2018-07-25 08:40:29 -0400

    Merged getCommand function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 //Speech interpretations
 var grammars = ["knight", "N",
 				"bishop", "B",
-				"rook", "R",
+				"rook", "brooke", "R",
 				"queen", "Q",
 				"king", "K",
 				"one", "1",
@@ -16,31 +16,23 @@ var grammars = ["knight", "N",
 				"six", "6",
 				"seven", "7",
 				"eight", "8",
-				"a", "b", "c", "d", "e", "f", "g", "h",
-				"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
-				"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
-				"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
-				"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
-				"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
-				"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
-				"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
-				"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
+				"a",
+				"b",
+				"c",
+				"d",
+				"e",
+				"f",
+				"g",
+				"h"]
 
-var moreGrammars = ["quincy", "Q c",
-					"queenie", "Q e"];
+var moreGrammars = ["quincy", "Qc",
+					"queenie", "Qe",
+					"88", "ad8"];
 
 //Speech commands
 var commands = ["N", "B", "R", "Q", "K",
 				"1", "2", "3", "4", "5", "6", "7", "8",
-				"a", "b", "c", "d", "e", "f", "g", "h",
-				"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
-				"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
-				"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
-				"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
-				"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
-				"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
-				"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
-				"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8"];
+				"a", "b", "c", "d", "e", "f", "g", "h"]
 
 //Notification themes
 var theme = {
